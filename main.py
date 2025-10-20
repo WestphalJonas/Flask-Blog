84c8bb29017fdeaf67dfc1885390695bc4b13939
Jonas Westphal <jonas@westphal.one>
2025-10-20 18:41:45 +0200

    Refactor main.py to set up Flask application and serve blog posts from JSON file

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
-def main():
-    print("Hello from flask-blog!")
+from flask import Flask, render_template
+import json
+
+
+app = Flask(__name__)
+
+def get_blog_posts():
+    with open("blog_posts.json", "r") as f:
+        return json.load(f)
+
+@app.route("/")
+def index():
+    blog_posts = get_blog_posts()
+    return render_template("index.html", posts=blog_posts)
 
 
 if __name__ == "__main__":
-    main()
+    app.run(host="0.0.0.0", port=5000, debug=True)
