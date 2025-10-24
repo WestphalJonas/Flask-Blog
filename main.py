54df25eda5e52e9a5c5069fe417ca9b79daa03e9
Jonas Westphal <jonas@westphal.one>
2025-10-25 01:16:32 +0200

    Add blog post functionality and enhance styles
    - Implemented functions to add and delete blog posts in main.py.
    - Updated routes to handle adding posts via a form and deleting posts by ID.
    - Enhanced styles in styles.css for better layout and responsiveness.
    - Refactored index.html to extend a layout template for improved structure.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template, request
 import json
 
 
@@ -8,10 +8,56 @@ def get_blog_posts():
     with open("blog_posts.json", "r") as f:
         return json.load(f)
 
+
+def save_blog_posts(posts):
+    with open("blog_posts.json", "w") as f:
+        json.dump(posts, f, indent=2)
+
+
+def add_blog_post(title, content, author):
+    posts = get_blog_posts()
+    if posts:
+        last_id = posts[-1]["id"]
+    else:
+        last_id = 0
+    new_post = {
+        "id": last_id + 1,
+        "author": author,
+        "title": title,
+        "content": content,
+        "likes": 0
+    }
+    posts.append(new_post)
+    save_blog_posts(posts)
+
+
 @app.route("/")
 def index():
     blog_posts = get_blog_posts()
     return render_template("index.html", posts=blog_posts)
+
+
+@app.route("/add", methods=["GET", "POST"])
+def add():
+    if request.method == "POST":
+        title = request.form.get("title")
+        content = request.form.get("content")
+        author = request.form.get("author")
+        
+        add_blog_post(title, content, author)
+        return redirect("/")
+    return render_template("add.html")
+
+
+@app.route('/delete/<int:post_id>')
+def delete(post_id):
+    blog_posts = get_blog_posts()
+    for post in blog_posts:
+        if post["id"] == post_id:
+            blog_posts.remove(post)
+            break
+    save_blog_posts(blog_posts)
+    return redirect("/")
 
 
 if __name__ == "__main__":
